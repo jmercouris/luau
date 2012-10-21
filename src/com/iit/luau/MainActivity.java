b413caf636862ad984c7fa54fcff9101f80da3be
John Mercouris <jmercouris@dhcp142.sewn1.iit.edu>
2012-10-20 20:04:15 -0500

    initial commit

---
+++ src/com/iit/luau/MainActivity.java
@@ -0,0 +1,20 @@
+package com.iit.luau;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
