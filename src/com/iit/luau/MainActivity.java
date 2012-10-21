eb109b66c5aa83c72be4e644405abe571466830c
John Mercouris <jmercouris@dhcp142.sewn1.iit.edu>
2012-10-20 21:37:12 -0500

    changed view text

--- src/com/iit/luau/MainActivity.java
+++ src/com/iit/luau/MainActivity.java
@@ -1,8 +1,10 @@
 package com.iit.luau;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Submit Button
+        TextView textView;
+        textView = (TextView) findViewById(R.id.textField);
+        textView.setText("lolol");
+
     }
 
     @Override
